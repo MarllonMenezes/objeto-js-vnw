6aa0183f4962d51f4b46ad17622231978b5cd36d
Marllon <marllon.menezes@g.globo>
2022-06-26 19:00:46 -0300

    exercicio 6 - estrutura de dados complexos (objeto)

---
+++ main.js
@@ -0,0 +1,95 @@
+// Crie um objeto que receba ao menos três propriedades sobre você.
+/* let pessoa = {
+    nome: 'Marllon',
+    idade: 18,
+    cidade: 'Rio de Janeiro',
+}; */
+
+
+// Adicione uma nova propriedade sem alterar seu objeto inicial.
+/*var pessoa = {
+    nome: 'Marllon',
+    idade: 18,
+    cidade: 'Rio de Janeiro',
+};
+
+ var myDataName = 'altura';
+ var myDataValue = '1.78m';
+pessoa [myDataName] = myDataValue; */
+
+
+
+// Remova uma propriedade desse objeto.
+/*let pessoa = {
+    nome: 'Marllon',
+    idade: 18,
+    cidade: 'Rio de Janeiro',
+};
+
+delete pessoa.cidade */
+
+
+
+//Mostre no console todas as propriedades desse objeto.
+/*
+let pessoa = {
+    nome: 'Marllon',
+    idade: 18,
+    cidade: 'Rio de Janeiro',
+};
+let mostraTudo = Object.keys(pessoa);
+console.log (mostraTudo)
+*/
+
+
+/*
+// Crie um array  chamado "cadastro" contendo ao menos 5 objetos. 
+//Cada objeto deve receber as seguintes propriedades: nome,  idade,  telefone, amigos. 
+//Na propriedade amigos, adicione ao menos 4 itens.
+
+let arrayCadastro = ['usuario1', 'usuario2', 'usuario3', 'usuario4', 'usuario5'];
+
+let usuario1 = {
+nome: 'Marllon',
+idade:18,
+telefone: '3245-1234',
+amigos: ['João', 'Maria', 'José', 'Pedro']
+};
+
+let usuario2 = {
+nome: 'João',
+idade:18,
+telefone: '3245-1232',
+amigos: ['Maria', 'José', 'Pedro', 'juca']
+};
+
+let usuario3 = {
+nome: 'Maria',
+idade:18,
+telefone: '3245-1233',
+amigos: ['José', 'Pedro', 'juca', 'marllon']
+};
+
+let usuario4 = {
+nome: 'José',
+idade:18,
+telefone: '3235-1234',
+amigos: ['Pedro', 'juca', 'marllon', 'joão']
+};
+
+let usuario5 = {
+nome: 'Pedro',
+idade:18,
+telefone: '3248-1234',
+amigos: ['juca', 'marllon', 'joão', 'maria']
+};
+
+
+// Mostre no console o nome de um amigo de cada lista.
+console.log(usuario1.amigos[0]);
+console.log(usuario2.amigos[0]);
+console.log(usuario3.amigos[0]);
+console.log(usuario4.amigos[0]);
+console.log(usuario5.amigos[0]);
+
+*/
